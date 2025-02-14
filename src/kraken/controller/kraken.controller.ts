247d52b0274271d8e61e9692d823d5abdee3b262
Tobin Frost <habib92m@gmail.com>
2025-02-14 01:45:21 +0100

    clean up

--- src/kraken/controller/kraken.controller.ts
+++ src/kraken/controller/kraken.controller.ts
@@ -1,34 +1,19 @@
-import {
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-
-import { Express } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
+import { ProductDto } from '../model/product.dto';
 
 @Controller('kraken')
 export class KrakenController {
   constructor(private readonly productService: ProductService) {}
+
   @Post('/')
-  @UseInterceptors(FileInterceptor('file'))
-  async get(@UploadedFile() file: Express.Multer.File) {
-    if (!file) {
-      return { message: 'Aucun fichier fourni' };
+  async getProducts(@Body() data: ProductDto[]) {
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        const product = data[i];
+        await this.productService.create(product);
+      }
+      return { message: 'Products inserted successfully' };
     }
-
-    /*  const p: Product = await this.productService.create({
-      name: 'test',
-      updated_at: new Date(),
-      prices: [1, 2],
-      rate: 2,
-      category: 'product',
-    }); */
-
-    //console.log('product : ', p);
-
-    return { message: 'Fichier Excel analysé avec succès' };
   }
 }
